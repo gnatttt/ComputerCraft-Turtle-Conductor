9961dd23c01f5b32ca5cfb8116369325cc6099ad
Nhat <58672082+gnatttt@users.noreply.github.com>
2022-12-19 01:46:39 -0500

    added protocol return and comments

--- repeater.lua
+++ repeater.lua
@@ -1,10 +1,15 @@
 rednet.open("left")
+term.clear()
+term.setCursorPos(1,1)
 
+
+--Receives signals from one end and relays it to the other end
+--This is essentially a repeater to extend signals
 function main()
 	while true do
-		id, send_to = rednet.receive()
+		id, send_to, protocol = rednet.receive()
 		print("System #" .. id .. " sent a cart to System #" .. send_to)
-		rednet.send(tonumber(send_to), id)
+		rednet.send(tonumber(send_to), id, protocol)
 		print("Sending a signal to System #" .. tonumber(send_to))
 	end
 end
